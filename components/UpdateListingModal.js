18ff94be0dbb2f83ffc44bf172ecc4c8b50611f6
Kage <romzhey@gmail.com>
2022-11-07 10:18:59 +0800

    first commit

---
+++ components/UpdateListingModal.js
@@ -0,0 +1,60 @@
+import { React, useState } from "react";
+import { useWeb3Contract } from "react-moralis";
+import { Modal, Input, useNotification } from "web3uikit";
+import nftMarketplaceAbi from "../constants/NftMarketplace.json";
+import { ethers } from "ethers";
+
+export default function UpdateListingModal({
+  nftAddress,
+  tokenId,
+  isVisible,
+  marketplaceAddress,
+  onClose,
+}) {
+  const dispatch = useNotification();
+
+  const [priceToUpdateListingWith, setPriceToUpdateListingWith] = useState(0);
+
+  const handleUpdateListingSuccess = async (tx) => {
+    await tx.wait(1);
+    dispatch({
+      type: "success",
+      message: "listing updated",
+      title: "Listing updated - please refresh (and move blocks)",
+      position: "topR",
+    });
+    onClose && onClose();
+    setPriceToUpdateListingWith("0");
+  };
+
+  const { runContractFunction: updateListing } = useWeb3Contract({
+    abi: nftMarketplaceAbi,
+    contractAddress: marketplaceAddress,
+    functionName: "updateListing",
+    params: {
+      nftAddress: nftAddress,
+      tokenId: tokenId,
+      newPrice: ethers.utils.parseEther(priceToUpdateListingWith || "0"),
+    },
+  });
+  return (
+    <Modal isVisible={isVisible} onCancel={onClose} onClose={onClose}>
+      <Input
+        label="Update listing price in L1 Currency (ETH)"
+        name="New listing price"
+        type="number"
+        onChange={(event) => {
+          setPriceToUpdateListingWith(event.target.value);
+        }}
+        onOk={() => {
+          updateListing({
+            onError: (eror) => {
+              console.log(error);
+            },
+            onSuccess: () => handleUpdateListingSuccess(),
+          });
+        }}
+      />
+    </Modal>
+  );
+}
